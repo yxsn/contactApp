cf63747d0aae515ee679095ca50ea2136386b7c2
Yasin N K <yasinnk15@gmail.com>
2023-07-17 01:51:46 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList"; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <AddContact></AddContact>
+      <ContactList></ContactList>
     </div>
   );
 }

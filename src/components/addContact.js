d667bff693f0af58422845883cc857edede22546
Yasin N K <yasinnk15@gmail.com>
2023-07-19 04:07:52 +0530

    commit

--- src/components/addContact.js
+++ src/components/addContact.js
@@ -1,3 +1,27 @@
 import React from "react";
 
- 
+ class AddContact extends React.Component{
+    render(){
+        return(
+            <div className = "addcontactUI">
+                <h3>Add Contact</h3>
+                <form className="addContactForm">
+                    <div className="field">
+                        <label>Name</label>
+                        <input type="text" name="name" placeholder="Name"/>
+                    </div>
+                    <div className="field">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="Email"/>
+                    </div>
+                    <div className="field">
+                        <label>Mobile</label>
+                        <input type="text" name="mobile" placeholder="Mobile"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+ }
+
+ export default AddContact;

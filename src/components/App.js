c18bf2c4014061bee6f87c939fd6c80a2d60def9
Yasin N K <yasinnk15@gmail.com>
2023-07-23 20:38:42 +0530

    contactList

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="uicontainer">
       <Header/>
       <AddContact/>
-      {/* <ContactList/> */}
+      <ContactList/>
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactList = () =>{
+    return(
+        <div className="celledList">
+            Contact List
+        </div>
+    );
+};
+
+export default ContactList;

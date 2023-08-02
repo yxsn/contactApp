b005bb22ec3cf54335bb71d0e9b3e31fce7e5844
Yasin N K <yasinnk15@gmail.com>
2023-08-03 02:17:48 +0530

    rendered contacts

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const ContactList = (props) =>{
-    console.log(props);
+    console.log(props); 
+
+    const renderContactList = props.contacts.map((contact)=>{
+        return(
+            <div className="item">
+                <div className="content">
+                    <div className="header">{contact.name}</div>
+                    <div>{contact.email}</div>
+                    <div>{contact.mobile}</div>
+                </div>
+                <i className="trashIcon"></i>
+            </div>
+        );
+    })
     return(
         <div className="celledList">
             <h3>Contact List</h3>
+            {renderContactList}
         </div>
     );
 };
